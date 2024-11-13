b0d7d3f847958278d29eb12f5543bb0463a9683d
NoemiP94 <m_emi94@hotmail.it>
2024-11-13 10:46:25 +0100

    fix images

--- characters/views.py
+++ characters/views.py
@@ -5,5 +5,22 @@ from .models import Character
 
 def characters_by_game_view(request, game_id):
     characters = Character.objects.filter(game_id=game_id)
-    characters_list = list(characters.values('name','charcClass', 'specialisation', 'where','title','affiliation','gender', 'race','romanceable', 'image' ,'game_id'))
+    characters_list = [ 
+    { 
+        'name': character.name, 
+        'class': character.charcClass,
+        'specialisation': character.specialisation,
+        'from': character.where,
+        'title': character.title,
+        'affiliation': character.affiliation,
+        'gender': character.gender,
+        'race': character.race,
+        'romanceable': character.romanceable,
+        'game_id': character.game_id, 
+        'image_url': request.build_absolute_uri(character.image.url) if character.image else None 
+
+        } 
+        for character in characters 
+        ]
     return JsonResponse(characters_list, safe=False)
+
--- da_be/settings.py
+++ da_be/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from decouple import config
 
@@ -136,3 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:4200"
 ]
+
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
