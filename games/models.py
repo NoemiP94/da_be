457ccc40ebc7277604913fa2efba6bde98aaefbe
NoemiP94 <m_emi94@hotmail.it>
2024-11-19 12:00:02 +0100

    add path in games

--- games/models.py
+++ games/models.py
@@ -12,17 +12,17 @@ class VideoGame(models.Model):
     # requisiti sistema
     pc_system_OS = models.TextField(default='pc_system_OS')
     pc_system_CPU = models.TextField(default='pc_system_CPU')
-    pc_system_RAM = models.CharField(max_length=500, default='pc_system_RAM')
+    pc_system_RAM = models.TextField(default='pc_system_RAM')
     pc_graphics_card = models.TextField(default='pc_graphics_card')
-    pc_graphics_memory = models.CharField(max_length=500, default='pc_graphics_memory')
-    pc_hard_drive = models.CharField(max_length=500, default='pc_hard_drive')
+    pc_graphics_memory = models.TextField(default='pc_graphics_memory')
+    pc_hard_drive = models.TextField(default='pc_hard_drive')
     pc_other = models.TextField(default='pc_other')
 
     mac_system_OS = models.TextField(default='mac_system_OS')
     mac_system_CPU = models.TextField(default='mac_system_CPU')
-    mac_system_RAM = models.CharField(max_length=500, default='mac_system_RAM')
+    mac_system_RAM = models.TextField(default='mac_system_RAM')
     mac_graphics_card = models.TextField(default='mac_graphics_card')
-    mac_graphics_memory = models.CharField(max_length=500, default='mac_graphics_memory')
-    mac_hard_drive = models.CharField(max_length=500, default='mac_hard_drive')
+    mac_graphics_memory = models.TextField(default='mac_graphics_memory')
+    mac_hard_drive = models.TextField(default='mac_hard_drive')
     mac_other = models.TextField(default='mac_other')
     def __str__(self): return self.title
--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path 
-from .views import game_list_view 
+from .views import game_detail_view, game_list_view 
 
 urlpatterns = [ 
     path('games/', game_list_view, name='games_list'), 
-    
+    path('detail/<int:game_id>/', game_detail_view, name='game_detail')
 ]
