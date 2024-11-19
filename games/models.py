95a37df9bf177b9935496654409f86a872af6d5c
NoemiP94 <m_emi94@hotmail.it>
2024-11-19 11:44:57 +0100

    add other fields in VideoGame

--- games/models.py
+++ games/models.py
@@ -4,5 +4,25 @@ from django.db import models
 class VideoGame(models.Model):
     title= models.CharField(max_length=300)
     year= models.IntegerField()
+    storyline = models.TextField(default='storyline')
+    gameplay = models.TextField(default='gameplay')
+    image = models.ImageField()
+    engine = models.CharField(max_length=300, default='engine')
+    game_mode = models.CharField(max_length=300, default='game_mode')
+    # requisiti sistema
+    pc_system_OS = models.TextField(default='pc_system_OS')
+    pc_system_CPU = models.TextField(default='pc_system_CPU')
+    pc_system_RAM = models.CharField(max_length=500, default='pc_system_RAM')
+    pc_graphics_card = models.TextField(default='pc_graphics_card')
+    pc_graphics_memory = models.CharField(max_length=500, default='pc_graphics_memory')
+    pc_hard_drive = models.CharField(max_length=500, default='pc_hard_drive')
+    pc_other = models.TextField(default='pc_other')
 
+    mac_system_OS = models.TextField(default='mac_system_OS')
+    mac_system_CPU = models.TextField(default='mac_system_CPU')
+    mac_system_RAM = models.CharField(max_length=500, default='mac_system_RAM')
+    mac_graphics_card = models.TextField(default='mac_graphics_card')
+    mac_graphics_memory = models.CharField(max_length=500, default='mac_graphics_memory')
+    mac_hard_drive = models.CharField(max_length=500, default='mac_hard_drive')
+    mac_other = models.TextField(default='mac_other')
     def __str__(self): return self.title
--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import Http404, JsonResponse
 from .models import VideoGame
 
 # Create your views here.
@@ -8,4 +8,33 @@ def game_list_view(request):
     game_list= list(games.values('id', 'title', 'year'))
     return JsonResponse(game_list, safe=False) 
 
-
+def game_detail_view(request, game_id):
+    try:
+        game = VideoGame.objects.get(id=game_id)
+        game_data = {
+            'id' : game.id,
+            'title': game.title,
+            'year': game.year,
+            'storyline' : game.storyline,
+            'gameplay': game.gameplay,
+            'engine': game.engine,
+            'game_mode': game.game_mode,
+            'pc_system_OS': game.pc_system_OS,
+            'pc_system_CPU': game.pc_system_CPU,
+            'pc_system_RAM': game.pc_system_RAM,
+            'pc_graphics_card': game.pc_graphics_card,
+            'pc_graphics_memory': game.pc_graphics_memory,
+            'pc_hard_drive': game.pc_hard_drive,
+            'pc_other': game.pc_other,
+            'mac_system_OS': game.mac_system_OS,
+            'mac_system_CPU': game.mac_system_CPU,
+            'mac_system_RAM': game.mac_system_RAM,
+            'mac_graphics_card': game.mac_graphics_card,
+            'mac_graphics_memory': game.mac_graphics_memory,
+            'mac_hard_drive': game.mac_hard_drive,
+            'mac_other': game.mac_other,
+            'image_url':request.build_absolute_uri(game.image.url) if game.image else None    
+        }
+        return JsonResponse(game_data)
+    except VideoGame.DoesNotExist:
+        raise Http404("VideoGame does not exist")
